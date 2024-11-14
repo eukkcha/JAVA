3e94d8f3296f42ef3ad78550dc97066f723569ca
JeonYuchan <eukkchan03@gmail.com>
2024-11-15 08:21:54 +0900

    Create Add.java

---
+++ Practice/02/Add.java
@@ -0,0 +1,11 @@
+public class Add
+  {
+    public class void main(String[] args) {
+      int x, y, sum;
+      x = 100;
+      y = 200;
+
+      sum = x + y;
+      System.out.println(sum);
+    }
+  }
